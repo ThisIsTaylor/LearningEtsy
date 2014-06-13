ad2edc91b8a4cd755a63df7b1583cb7283537ca5
ThisIsTaylor <taylor.mcgregor@detroitlabs.com>
2014-06-13 11:08:26 -0400

    Initial Commit

---
+++ Etsy/TEMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEMAppDelegate.h
+//  Etsy
+//
+//  Created by DL Admin on 6/13/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Etsy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Etsy
+//
+//  Created by DL Admin on 6/13/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TEMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TEMAppDelegate class]));
+    }
+}
