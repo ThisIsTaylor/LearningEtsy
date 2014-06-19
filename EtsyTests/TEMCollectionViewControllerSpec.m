9a7be0b03eaa7385a48f614afc9521b6e8db3004
ThisIsTaylor <taylor.mcgregor@detroitlabs.com>
2014-06-19 14:19:44 -0400

    Refactored test added test for productImageView

--- EtsyTests/TEMCollectionViewControllerSpec.m
+++ EtsyTests/TEMCollectionViewControllerSpec.m
@@ -9,14 +9,17 @@
 #import <Kiwi/Kiwi.h>
 #import "TEMCollectionViewController.h"
 #import "TEMProductCell.h"
+#import <AFNetworking/UIImageView+AFNetworking.h>
 
 SPEC_BEGIN(TEMCollectionViewControllerSpec)
 
 	describe(@"TEMCollectionViewController", ^{
 		__block TEMCollectionViewController *collectionViewController;
+        __block TEMProductCell *cell;
         
         beforeEach(^{
             collectionViewController = [[TEMCollectionViewController alloc] init];
+            cell = [[TEMProductCell alloc] init];
         });
         
         it(@"should have an etsyService property", ^{
@@ -24,10 +27,6 @@ SPEC_BEGIN(TEMCollectionViewControllerSpec)
         });
         
         it(@"should update cell productLabel with the product names", ^{
-            [collectionViewController.productCollecionView registerNib:[UINib nibWithNibName:@"TEMProductCell" bundle:nil] forCellWithReuseIdentifier:@"EtsyProductCell"];
-            
-            TEMProductCell *cell = [[TEMProductCell alloc] init];
-            
             if (collectionViewController.storeItems){
             cell.productLabel = [UILabel nullMock];
             NSString *sampleProduct = @"A Product Title";
@@ -39,8 +38,22 @@ SPEC_BEGIN(TEMCollectionViewControllerSpec)
             void (^capturedSuccessBlock)(NSString *) = spy.argument;
             capturedSuccessBlock(sampleProduct);
             }
-
         });
+        
+        it(@"should update cell productImageView to display product images", ^{
+            if (collectionViewController.storeItems){
+                cell.productImageView = [UIImageView nullMock];
+                NSString *sampleImageURL = @"this is an image URL";
+                
+                [[cell.productImageView should] receive:@selector(setImageWithURL:) withArguments:sampleImageURL];
+                
+                KWCaptureSpy *spy = [collectionViewController.etsyService captureArgument:@selector(getEtsyStoreItems:) atIndex:0];
+                [collectionViewController viewDidLoad];
+                void (^capturedSuccessBlock)(NSString *) = spy.argument;
+                capturedSuccessBlock(sampleImageURL);
+            }
+        });
+
 	});
-	
+
 SPEC_END
